78fc61e75b774e363736a555b15fa2bb279cabe8
Robert Kolner <robert.kolner@gmail.com>
2022-08-22 10:45:55 +0200

    fix: Update both packages

--- package-left/index.js
+++ package-left/index.js
@@ -1,2 +1,3 @@
 console.log("This is package-left output");
 console.log("I'm line number 2");
+console.log("I'm line number 3");
--- package-right/index.js
+++ package-right/index.js
@@ -1,2 +1,3 @@
 console.log("This is package-right");
 console.log("And now it's updated");
+console.log("I'm line number 3");
